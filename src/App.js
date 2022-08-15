94b7f7b67563ae31186707c0fe275299b11851d7
jung im <jungim4201@gamil.com>
2022-08-15 19:24:21 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+//3개 페이지 홈,무비,영화상세페이지
+//2. 홈페이지에서 배너를 볼 수 있다.
+//3. 3가지섹션의 영화를 볼 수 있다.(popular, top rated, upcoming)
+//4. 각영화의 마우스를 올려두면 제목, 장르, 점수, 인기도, 청불여부
+//5.영화를 슬라이드로 넘기면서 다른 영화를 볼 수있다.
+
+//6. 영화디테일 페이지에서 영화에 대한 디테일 정보를 볼 수있다.(포스터,제목,줄거리,점수,인기도등)
+//7. trailer를 누르면 예고편을 볼 수있다.
+//8. 영화의 리뷰도 볼 수 있따.
+//9. 관련된 영화(related movie)도 볼 수있다.
+//10. 영화 검색을 할 수 있다.
+//11. 영화 정렬 , 필터링 할 수있다.
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
 }
